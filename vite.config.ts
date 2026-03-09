cc939686c8424d820831c7983d3853a8d2730c88
Rafi <mr.k779@outlook.com>
2026-03-09 13:07:37 -0400

    fix: update base path for root GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig(({}) => {
   return {
-    base: "/Website/",
+    base: "/",
     server: {
       port: 3000,
       host: "0.0.0.0",
